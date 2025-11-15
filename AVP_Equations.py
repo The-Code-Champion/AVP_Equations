260560edd6b903a77867b4e6d1a37f423e4f1de4
Jacob Alley <jaalley2@ncsu.edu>
2025-11-15 12:31:45 -0500

    restructure fixes

--- AVP_Equations.py
+++ AVP_Equations.py
@@ -8,15 +8,14 @@ app = Flask(__name__)
 def equation_web():
     if request.method == 'POST' and request.form.get('equation_type') == 'CD_Eq':
         return redirect(url_for('CD_val'))
-
     return render_template('Main_Page.html')
 
 @app.route('/CD_val', methods=['GET','POST'])
 def CD_val():
     if request.method == 'POST':
-        K_val = request.form('K')
-        CDo_val = request.form('CDo')
-        CL_val = request.form('CL')
+        K_val = float(request.form('K'))
+        CDo_val = float(request.form('CDo'))
+        CL_val = float(request.form('CL'))
 
         CD_val = CDo_val + K_val*(CL_val)**2
 
